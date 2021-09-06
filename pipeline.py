6b8d7193724ca5d4abd7edb8a3c0f40e5b59313b
THIAGO VALE <77752211+thiago-vale@users.noreply.github.com>
2021-09-05 23:46:04 -0300

    Delete pipeline.py

--- pipeline.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Super simple pipeline chaining the crawler, cleaning,
-# removing outliers and training scripts
-
-# os.system("python crawler.py")
-os.system("python cleaning.py")
-os.system("python removing_outliers.py")
-os.system("python model_training.py")
