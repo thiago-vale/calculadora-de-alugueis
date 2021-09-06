5b43f2455405ee6aef99120b22473244b6e30d60
THIAGO VALE <77752211+thiago-vale@users.noreply.github.com>
2021-09-05 23:47:35 -0300

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -7,7 +7,6 @@ Created on Thu Sep  2 16:21:12 2021
 
 import os
 
-os.system("Coletando-dados-calculadora.py")
-os.system("Tratando os dados.py")
+os.system("Coletando-dados.py")
+os.system("Tratando-os-dados.py")
 os.system("Limpando-outliers.py")
-#os.system("Treinando-Modelo ")
